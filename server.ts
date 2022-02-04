bc8d3905ce416dfb59fb13918c7296ac147ae4e4
Ratneshkummar Pandey <pandeyratneshkumar03@gmail.com>
2022-02-05 00:07:00 +0530

    stripe-api-add

--- server.ts
+++ server.ts
@@ -1,44 +1,87 @@
 require("dotenv").config();
-import { ApolloServer, AuthenticationError } from "apollo-server";
+const express = require("express");
+// import { ApolloServer, AuthenticationError } from "apollo-server";
 import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
-import { ApolloServerPluginLandingPageDisabled } from "apollo-server-core";
-const bcrypt = require("bcrypt");
-let server = null;
+// import { ApolloServerPluginLandingPageDisabled } from "apollo-server-core";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+// const bcrypt = require("bcrypt");
+// let server = null;
+const expressPlayground =
+  require("graphql-playground-middleware-express").default;
+const app = express();
+const stripe = require("stripe")(
+  "sk_test_51KNJ2FSBPFLa6VsBCMITKY4e4Oq4V3yl7FMQGRYf1PYXMxKUCFjCN5yjrQ72Znb6yiOduJtEiTr253mIO3rnRiBQ00TH5btEGA"
+);
+import * as admin from "firebase-admin";
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    clientEmail: process.env.CLIENT_EMAIL,
+  }),
+});
 
-if (!server) {
-  server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-      console.log(req.headers);
-      const arrayAuth = req.headers.authorization?.split(" ");
-      if (arrayAuth && arrayAuth.includes("bearer")) {
-        const res = await bcrypt.compare(
-          arrayAuth[1],
-          process.env.HASH_PASSWORD
-        );
-        if (!res) {
-          return {
-            authorised: false,
-            err: true,
-          };
-        } else {
-          return {
-            authorised: true,
-            err: false,
-          };
-        }
-      } else {
-        return {
-          authorised: false,
-          err: true,
-        };
-      }
+// if (!server) {
+//   server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: async ({ req }) => {
+//       console.log(req.headers.authorization);
+//       const arrayAuth = req.headers.authorization?.split(" ");
+//       return {
+//         authorised: true,
+//         err: false,
+//       };
+//     },
+//     // plugins: [ApolloServerPluginLandingPageDisabled],
+//   });}
+const verifyIdToken = async (idToken: string) => {
+  return await admin.auth().verifyIdToken(idToken);
+};
+const loggingMiddleware = (req: any, res: any, next: any) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send("You are not authorised to make any request.");
+  }
+  const idToken = authorization.split(" ")[1];
+  verifyIdToken(idToken)
+    .then((decodedToken) => {
+      console.log(decodedToken.uid);
+      return next();
+    })
+    .catch((err) => {
+      return res
+        .status(401)
+        .send("You are not authorised to make any request.");
+    });
+};
+app.use(loggingMiddleware);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(typeDefs),
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+app.post("/checkout", async (req: any, res: any) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 10,
+    currency: "eur",
+    // customer: customer.id,
+    automatic_payment_methods: {
+      enabled: true,
     },
-    // plugins: [ApolloServerPluginLandingPageDisabled],
   });
-  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+
+  return res.json({
+    paymentIntent: paymentIntent.client_secret,
+    publishableKey: process.env.PUBLISABLE_KEY,
   });
-}
+});
+app.listen(process.env.PORT || 4000, () => {
+  console.log("server is running...");
+});
--- typeDefs.ts
+++ typeDefs.ts
@@ -1,6 +1,6 @@
 import { gql } from "apollo-server";
 
-export default gql`
+export default `
   type Vocab {
     german: String!
     english: String!
